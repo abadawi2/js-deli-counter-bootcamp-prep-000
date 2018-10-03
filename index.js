d29b2fbcd199c798d5434c7a8683196cc13b8a71
abadawi2 <abadawi@terpmail.umd.edu>
2018-10-03 01:35:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function currentLine(katzDeliLine) {
     var name = katzDeliLine[i]
     string +=[ (i+1) + '. '+name+', '];
   }
-  var finalString = string.substring(0, string.length)
+  var finalString = string.substring(0, string.length-2)
     return finalString
   }
