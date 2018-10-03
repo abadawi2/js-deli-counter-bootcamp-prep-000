7e113acfa395ffece8b7b43a0f159cf6235dbc74
abadawi2 <abadawi@terpmail.umd.edu>
2018-10-03 01:32:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length===0){
     return ("The line is currently empty.")
   }
-  var string = ['The line is currently: '];
+  var string = 'The line is currently: ';
   for (var i = 0; i < katzDeliLine.length; i++){
     var name = katzDeliLine[i]
     string +=[ (i+1) + '. '+name+', '];
